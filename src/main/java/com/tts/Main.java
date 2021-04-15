d8288c135ce3536bafb47b1b655d39019521e9a6
Laura Aleman <79146743+lauraaleman3@users.noreply.github.com>
2021-04-15 13:54:52 -0500

    commit 2

--- src/main/java/com/tts/Main.java
+++ src/main/java/com/tts/Main.java
@@ -7,54 +7,54 @@ public class Main {
 
         Scanner scanner = new Scanner(System.in);
 
-//        System.out.println("Please enter a number.");
-//        int numOne = Integer.parseInt(scanner.next());
-//
-//        System.out.println("Please enter a second number.");
-//        int numTwo = Integer.parseInt(scanner.next());
-//
-//        System.out.println("Please enter a third number.");
-//        int numThree = Integer.parseInt(scanner.next());
-//
-//        System.out.println("Please enter a fourth number.");
-//        int numFour = Integer.parseInt(scanner.next());
-//
-//        System.out.println("Please enter a fifth number.");
-//        int numFive = Integer.parseInt(scanner.next());
-//
-//        List<Integer> intList = new ArrayList<>();
-//
-//        intList.add(numOne);
-//        intList.add(numTwo);
-//        intList.add(numThree);
-//        intList.add(numFour);
-//        intList.add(numFive);
-//
-//        System.out.println("Numbers in an array list: " + intList);
+        System.out.println("Please enter a number.");
+        int numOne = Integer.parseInt(scanner.next());
 
-// // For Sum calculation
+        System.out.println("Please enter a second number.");
+        int numTwo = Integer.parseInt(scanner.next());
 
-//        int sum = numOne + numTwo + numThree + numFour + numFive;
-//        System.out.println("Sum: " + sum);
+        System.out.println("Please enter a third number.");
+        int numThree = Integer.parseInt(scanner.next());
 
-// // For Product calculation
-//        int product = numOne * numTwo * numThree * numFour * numFive;
-//        System.out.println("Product: " + product);
+        System.out.println("Please enter a fourth number.");
+        int numFour = Integer.parseInt(scanner.next());
 
- // For Smallest number
-//        int min = intList.get(0);
-//        for (int i = 1; i < intList.size(); i++) {
-//            int curr = intList.get(i);
-//            if (curr < min) {
-//                min = curr;
-//            }
-//        }
-//        System.out.println ("The smallest number is: " + min);
+        System.out.println("Please enter a fifth number.");
+        int numFive = Integer.parseInt(scanner.next());
 
-// // For Largest number
-//        List<Integer> sortedList = new ArrayList<>(intList);
-//        int max = sortedList.get(sortedList.size()-1);
-//        System.out.println ("The largest number is: " + max);
+        List<Integer> intList = new ArrayList<>();
+
+        intList.add(numOne);
+        intList.add(numTwo);
+        intList.add(numThree);
+        intList.add(numFour);
+        intList.add(numFive);
+
+        System.out.println("Numbers in an array list: " + intList);
+
+ // For Sum calculation
+
+        int sum = numOne + numTwo + numThree + numFour + numFive;
+        System.out.println("Sum: " + sum);
+
+ // For Product calculation
+        int product = numOne * numTwo * numThree * numFour * numFive;
+        System.out.println("Product: " + product);
+
+ // // For Smallest number
+        int min = intList.get(0);
+        for (int i = 1; i < intList.size(); i++) {
+            int curr = intList.get(i);
+            if (curr < min) {
+                min = curr;
+            }
+        }
+        System.out.println ("The smallest number is: " + min);
+
+ // For Largest number
+        List<Integer> sortedList = new ArrayList<>(intList);
+        int max = sortedList.get(sortedList.size()-1);
+        System.out.println ("The largest number is: " + max);
 
 // // Car Dealer
         HashMap<String, String> carMap = new HashMap<>();
